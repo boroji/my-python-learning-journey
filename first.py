6d21a158d0d6e0a18800eac3d8b9102e0b1e7a62
Hossein <hosseinboroji@gmail.com>
2025-05-29 20:11:04 -0400

    finished the exercise

--- first.py
+++ first.py
@@ -1,1 +1,26 @@
-help("keywords")
+# Create a variable called secret_num and set it to any number you'd like:
+
+# Increase secret_num by 1
+
+# Next, take the new number and double it
+
+# Next, take the new number and add four to it
+
+#Next, take the new number and divide by two
+
+# Next, take the new number and subtract your original secret number from it
+
+# Print the final result
+
+
+# To see my magical prediction, scroll down...
+
+secret_num = 8  # You can set this to any number you'd like
+secret_num += 1  # Increase secret_num by 1
+secret_num *=2  # Double the new number
+secret_num += 4  # Add four to the new number
+secret_num /= 2  # Divide the new number by two
+final_result = secret_num - 7  # Subtract the original secret number
+print(final_result)  # Print the final result
+# The final result will always be 3, regardless of the initial secret_num
+
